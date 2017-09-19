39974f01c7da59fad76941b2ad3fa6684fa06eb2
Samuel Santibout <santibout@yahoo.com>
2017-09-19 15:13:37 -0700

    Added people table long with select all stored proc

---
+++ Z.E.R.O-1.database/StoredProcs/People_SelectAll.sql
@@ -0,0 +1,19 @@
+﻿CREATE PROCEDURE [dbo].[People_SelectAll]
+AS
+/* 
+EXECUTE [dbo].[People_SelectAll]
+*/
+BEGIN
+	SELECT
+		[Id],
+		[FirstName],
+		[MiddleInitial],
+		[LastName],
+		[DateOfBirth],
+		[CreatedDate],
+		[ModifiedDate],
+		[ModifiedBy]
+
+	FROM 
+		[dbo].[People]
+END
